9da3451f096462b34609642ae1f8d0756bf9c04d
shrumping <goneShrumping@gmail.com>
2026-04-19 16:40:43 -0500

    Create main.h for main.c
    Add header file for main.c with function declaration

---
+++ include/main.h
@@ -0,0 +1,12 @@
+/*
+*  main.h
+*  
+*  created: apr 19, 2026
+*  author: Eric Barber
+*  description: this is the header file for main.c
+*/
+
+/*----includes-----*/
+
+/*-----Function Definitions-----*/
+int main(void);
